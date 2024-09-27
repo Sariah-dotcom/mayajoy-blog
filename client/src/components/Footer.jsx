0ddfc861f111b660bbde66dc4a62bc33c029a111
Sariah Jackson <sarijah13@gmail.com>
2024-09-27 12:43:59 -0400

    create sign in api route

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,41 +5,42 @@ import { MdEmail } from "react-icons/md";
 
 export default function Footer() {
     const year = new Date().getFullYear();
-  return (
-    <div>
-        <footer>
-            <div className="py-5 px-8 border-t-1 border-dark-green">
+    
+    return (
+        <div>
+            <footer>
+                <div className="py-5 px-8 border-t-1 border-dark-green">
 
-                <div className="flex justify-between items-center">
-                    <div className="">
-                        <img src="/mj-logo.png" alt="logo" className='h-36'/>
+                    <div className="flex justify-between items-center">
+                        <div className="">
+                            <img src="/mj-logo.png" alt="logo" className='h-36'/>
+                        </div>
+
+                        <div className="flex gap-10 font-semibold">
+                            <Link to='/' className="text-dark-green hover:text-med-green">Home</Link>
+                            <Link to='/about' className="text-dark-green hover:text-med-green">About</Link>
+                            <Link to='/posts' className="text-dark-green hover:text-med-green">Blog Posts</Link>
+                        </div>
                     </div>
 
-                    <div className="flex gap-10 font-semibold">
-                        <Link to='/' className="text-dark-green hover:text-med-green">Home</Link>
-                        <Link to='/about' className="text-dark-green hover:text-med-green">About</Link>
-                        <Link to='/posts' className="text-dark-green hover:text-med-green">Blog Posts</Link>
+                    <hr className="border-med-green" />
+
+                    <div className="flex items-center justify-end gap-10 py-5 text-med-green">
+                        <div className="flex gap-1">
+                            <p>©</p>
+                            {year}
+                        </div>
+
+                        <div className="flex gap-5">
+                            <FaPinterest />
+                            <FaInstagramSquare />
+                            <MdEmail />
+                        </div>
                     </div>
+
                 </div>
 
-                <hr className="border-med-green" />
-
-                <div className="flex items-center justify-end gap-10 py-5 text-med-green">
-                    <div className="flex gap-1">
-                        <p>©</p>
-                        {year}
-                    </div>
-
-                    <div className="flex gap-5">
-                        <FaPinterest />
-                        <FaInstagramSquare />
-                        <MdEmail />
-                    </div>
-                </div>
-
-            </div>
-
-        </footer>
-    </div>
-  )
+            </footer>
+        </div>
+    )
 }
